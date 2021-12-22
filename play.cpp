7da120112ccfe4d329b776587f0e8172b72407a1
Swasthik Shetty <62538932+swasthikshetty10@users.noreply.github.com>
2021-12-22 19:13:24 +0530

    Linux version

--- play.cpp
+++ play.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
-#include <windows.h>
 #include "SudokuGenerator.cpp"
 #include "ScoreHandler.cpp"
 #define N 9
 using namespace std;
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // To set text color
 // Sudoku Game
 class Sudoku : virtual SudokuSolver
 {
@@ -121,13 +119,19 @@ public:
         solved = 0;
         for (int i = 0; i < 9; i++)
         {
-            SetConsoleTextAttribute(hConsole, 3); // setting Light Aqua as text color for console
             for (int j = 0; j < 9; j++)
             {
                 if ((i == 0 | i == 3 | i == 6) && j == 0)
-                    cout << " -------------------------\n";
+                    cout << "\033[1;36m "
+                         << "-------------------------"
+                         << "\033[0m"
+                         << endl;
+                // setting cyan as text color
                 if (j == 0 | j == 3 | j == 6)
-                    cout << " | ";
+                    cout << "\033[1;36m"
+                         << " | "
+                         << "\033[0m";
+                // setting cyan as text color
                 else
                     cout << " ";
                 if (grid[i][j])
@@ -135,29 +139,32 @@ public:
                     if (grid[i][j] == (puzzle->grid)[i][j])
                     {
                         if (grid[i][j] == Qgrid[i][j])
-                            SetConsoleTextAttribute(hConsole, 7); // setting white as text color
+                            cout << grid[i][j];
                         else
-                            SetConsoleTextAttribute(hConsole, 2); // setting green as text color
-                        cout << grid[i][j];
+                            cout << "\033[1;32m" << grid[i][j] << "\033[0m"; // setting green as text color
+
                         solved++;
                     }
                     else
                     {
-                        SetConsoleTextAttribute(hConsole, 4); // setting red as text color
-                        cout << grid[i][j];
+                        cout << "\033[1;31m" << grid[i][j] << "\033[0m"; // setting red as text color
                     }
                 }
                 else
                 {
-                    SetConsoleTextAttribute(hConsole, 3);
-                    cout << '-';
+                    cout << "\033[1;36m"
+                         << "-"
+                         << "\033[0m"; // setting cyan as text color
                 }
-                SetConsoleTextAttribute(hConsole, 3);
             }
-            cout << " | \n";
+            cout << "\033[1;36m"
+                 << " | "
+                 << "\033[0m\n";
         }
-        cout << " -------------------------\n";
-        SetConsoleTextAttribute(hConsole, 7);
+        cout << "\033[1;36m"
+             << " -------------------------"
+             << "\033[0m"
+             << endl; // setting cyan as text color
     }
     // function  check if entered location valid
     bool isValid(int row, int col)
